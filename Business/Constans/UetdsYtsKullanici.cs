2f1a540c6c40d95c26ef4da3c4de582754f93016
Hüseyin ÇALIŞKAN <77413390+husseyin@users.noreply.github.com>
2023-01-13 10:59:08 +0300

    Add project files.

--- Business/Constans/UetdsYtsKullanici.cs
+++ Business/Constans/UetdsYtsKullanici.cs
@@ -7,10 +7,7 @@ using System.Threading.Tasks;
 namespace Business.Constans
 {
     public static class UetdsYtsKullanici
-    {
-        //public static string KullaniciAdi = "20588";
-        //public static string Parola = "MRRNKFWUUU";
-
+    {       
         public static string KullaniciAdi = "999999";
         public static string Parola = "999999testtest";
     }
